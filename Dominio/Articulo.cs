419769180182a026224967e51f5a2df1d9c9cf44
daniguillen <72428160+daniguillen@users.noreply.github.com>
2024-05-11 00:11:10 -0300

    base de dato
    se pego la base de datos

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int IDMarca { get; set; }
+        public string Marca { get; set; }   
+        public int IDCategoria { get; set; }
+        public string Categoria { get; set; }
+        public SqlMoney Precio { get; set; }
+        public string Imagen { get; set; }
+
+        public void Cargar()
+        {
+
+        }
+    }   
+}
